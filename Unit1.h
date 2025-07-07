71ecedb2b7c982b0dbe6734f10d575b30bc4f687
nao <jake.burst@gmail.com>
2025-07-07 16:24:26 +0900

    a button added

--- Unit1.h
+++ Unit1.h
@@ -6,10 +6,15 @@
 #include <System.Classes.hpp>
 #include <FMX.Controls.hpp>
 #include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE で管理されるコンポーネント
+	TButton *Button1;
+	void __fastcall Button1Click(TObject *Sender);
 private:	// ユーザー宣言
 public:		// ユーザー宣言
 	__fastcall TForm1(TComponent* Owner);
